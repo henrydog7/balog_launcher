b465e64f17bf255f0d09a35cc60147aa143ba61a
henrydog7 <47285651+henrydog7@users.noreply.github.com>
2021-12-27 15:26:50 -0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/balog_launcher/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.balog_launcher
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+void main() {
+  runApp(BalogLauncher());
+}
+
+class BalogLauncher extends StatelessWidget {
+  const BalogLauncher({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
