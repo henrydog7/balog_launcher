1ea60fded95dd099d625ca84371ebd295ad2bd41
henrydog7 <balog7lucas@gmail.com>
2021-12-29 15:04:00 -0500

    V3

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 bool lightsOn = false;
+double globePosX = 0;
+double globePosY = 0;
 
 void main() => runApp(const MyApp());
 
@@ -13,13 +15,15 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  void refresh() => setState(() {});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(primarySwatch: Colors.green),
-        home: const Scaffold(
-          body: MyHome(),
+        home: Scaffold(
+          body: const MyHome(),
+          appBar: AppBar(leading: const MyAppBar()),
         ));
   }
 }
@@ -32,41 +36,43 @@ class MyHome extends StatefulWidget {
 }
 
 class _MyHomeState extends State<MyHome> {
-  double dragDistance = 0;
+  late Function(String) refresh;
+
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-
-      onPanUpdate: (details) => setState((){
+      onPanUpdate: (details) {
         if (kDebugMode) {
-          print(details.delta.dy);
+          print(details.globalPosition);
         }
-        dragDistance = details.delta.dy;
-        }),
-
+        globePosX = details.globalPosition.dx;
+        globePosY = details.globalPosition.dy;
+        MyApp.refresh();
+      },
       child: Container(
         constraints: const BoxConstraints.expand(),
-        color: HSLColor.fromAHSL(1, 120, 1.0, 1).toColor(),
+        color: HSLColor.fromAHSL(
+                1,
+                (globePosY / MediaQuery.of(context).size.height) * 360,
+                1,
+                globePosX / MediaQuery.of(context).size.width)
+            .toColor(),
         child: Container(
           alignment: FractionalOffset.center,
           child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Icon(
-                    lightsOn ? Icons.flashlight_on : Icons.flashlight_off,
-                    color: lightsOn ? Colors.yellow.shade600 : Colors.grey,
-                    size: 60,
+                Container(
+                  width: 300.0,
+                  height: 300.0,
+                  decoration: const BoxDecoration(
+                    color: Colors.lightGreen,
+                    shape: BoxShape.circle,
                   ),
                 ),
-                Container(
-                  color: Colors.yellow.shade600,
-                  padding: const EdgeInsets.all(8),
-                  child: Text(dragDistance.toString()),//lightsOn ? "On" : "Off"),
-                ),
-              ]
-          ),
+
+              ]),
         ),
       ),
     );
@@ -75,4 +81,31 @@ class _MyHomeState extends State<MyHome> {
 
 void changeLights() {
   lightsOn = !lightsOn;
+}
+class MyAppBar extends StatefulWidget {
+  const MyAppBar({Key? key}) : super(key: key);
+
+  @override
+  _MyAppBarState createState() => _MyAppBarState();
+}
+
+class _MyAppBarState extends State<MyAppBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Container(
+          color: Colors.yellow.shade500,
+          padding: const EdgeInsets.all(8),
+          child: Text("Y: " + globePosY.toString()),
+        ),
+        Container(
+          color: Colors.yellow.shade600,
+          padding: const EdgeInsets.all(8),
+          child: Text(
+              "X: " + globePosX.toString()), //lightsOn ? "On" : "Off"),
+        ),
+      ],
+    );
+  }
 }
