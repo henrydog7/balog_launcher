dbef904187b199723e3c9396d723e84c356a7452
henrydog7 <balog7lucas@gmail.com>
2021-12-31 10:29:02 -0500

    V4

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 bool lightsOn = false;
 double globePosX = 0;
 double globePosY = 0;
+final GlobalKey _widgetKey = GlobalKey();
 
 void main() => runApp(const MyApp());
 
@@ -15,11 +17,8 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
   void _refresh() {
-    setState(() {
-
-    });
+    setState(() {});
   }
 
   @override
@@ -28,9 +27,8 @@ class _MyAppState extends State<MyApp> {
         debugShowCheckedModeBanner: false,
         theme: ThemeData(primarySwatch: Colors.green),
         home: Scaffold(
-          body: MyHome(onChanged: _refresh),
+          body: MyHome(refresh: _refresh),
           appBar: AppBar(
-
             title: Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
@@ -56,10 +54,10 @@ class MyHome extends StatefulWidget {
   @override
   const MyHome({
     Key? key,
-    required this.onChanged,
+    required this.refresh,
   }) : super(key: key);
 
-  final Function onChanged;
+  final Function refresh;
 
   @override
   _MyHomeState createState() => _MyHomeState();
@@ -72,41 +70,39 @@ class _MyHomeState extends State<MyHome> {
       onPanUpdate: (details) {
         if (kDebugMode) {
           print(details.globalPosition);
+          print(_widgetKey.currentContext?.size?.width);
         }
-        globePosX = details.globalPosition.dx;
-        globePosY = details.globalPosition.dy;
-        widget.onChanged();
+        globePosX = details.localPosition.dx;
+        globePosY = details.localPosition.dy;
+        widget.refresh();
       },
       child: Container(
         constraints: const BoxConstraints.expand(),
         color: HSLColor.fromAHSL(
-            1,
-            (globePosY / MediaQuery
-                .of(context)
-                .size
-                .height) * 360,
-            1,
-            globePosX / MediaQuery
-                .of(context)
-                .size
-                .width)
+                1,
+                globePosY < < _widgetKey.currentContext?.size?.width.toInt() ? (globePosY / MediaQuery.of(context).size.height) * 360,
+                1,
+                globePosX / MediaQuery.of(context).size.width)
             .toColor(),
         child: Container(
           alignment: FractionalOffset.center,
           child: Column(
               mainAxisAlignment: MainAxisAlignment.end,
               children: <Widget>[
-                Container(
-                  width: 600.0,
-                  height: 300.0,
-                  decoration: const BoxDecoration(
-                    color: Colors.lightGreen,
-                    shape: BoxShape.circle,
-                  ),
-                ),
+                // Container(
+                //   width: 600.0,
+                //   height: 300.0,
+                //   decoration: const BoxDecoration(
+                //     color: Colors.lightGreen,
+                //     shape: BoxShape.circle,
+                //   ),
+                // ),
                 CustomPaint(
-                  size: Size(100, 100),
-                  painter: MyPainter(),
+                  size: Size(
+                      MediaQuery.of(context).size.width.toDouble(),
+                      (MediaQuery.of(context).size.height.toDouble()) / 2
+                  ),
+                  painter: CurvedPainter(),
                 )
               ]),
         ),
@@ -115,26 +111,48 @@ class _MyHomeState extends State<MyHome> {
   }
 }
 
-class MyPainter extends CustomPainter {
+// class MyPainter extends CustomPainter {
+//   @override
+//   void paint(Canvas canvas, Size size) {
+//     canvas.drawArc(
+//       const Offset(0, 0) & const Size(100, 100),
+//       0.0,
+//       180 ,
+//       true,
+//       Paint(),
+//     );
+//   }
+//
+//   @override
+//   bool shouldRepaint(CustomPainter oldDelegate) => false;
+//
+// }
+class CurvedPainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
-    canvas.drawArc(
-      const Offset(0, 0) & const Size(100, 100),
-      0.0,
-      180.0  ,
-      true,
-      Paint(),
-    );
+    var paint = Paint()
+      ..color = Colors.teal
+      ..strokeWidth = 15;
+
+    var path = Path();
+
+    path.moveTo(0, size.height * 0.7);
+    path.quadraticBezierTo(size.width * 0.25, size.height * 0.7,
+        size.width * 0.5, size.height * 0.8);
+    path.quadraticBezierTo(size.width * 0.75, size.height * 0.9,
+        size.width * 1.0, size.height * 0.8);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+
+    canvas.drawPath(path, paint);
   }
 
   @override
-  bool shouldRepaint(CustomPainter oldDelegate) => false;
-
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
 }
-
 
 void changeLights() {
   lightsOn = !lightsOn;
 }
-
-
